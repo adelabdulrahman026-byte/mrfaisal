6ccf47d09632c509d9f0b9675dd77172b6f49f39
adelabdulrahman026-byte <adelabdulrahman026@gmail.com>
2025-09-02 18:38:27 +0300

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,8 +1,1 @@
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-NQTPWYLG8Z"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-NQTPWYLG8Z');
-</script>
