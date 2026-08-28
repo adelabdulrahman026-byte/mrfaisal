95c2d8d57f97f9f19a1cafa46f4a9b03ead92734
Abdulrahman Adel <adelabdulrahman026@gmail.com>
2026-08-28 18:56:07 +0300

    Upload project files

---
+++ script.js
@@ -0,0 +1,9 @@
+function goSection(section) {
+  if (section === "business") {
+    // تقدر تغير الرابط ده لاحقًا لقسم إدارة الأعمال
+    alert("تم الدخول إلى قسم إدارة الأعمال 📊");
+    // window.location.href = "business.html";
+  } else if (section === "geo") {
+    window.location.href = "https://www.mrfaisal.net/start";
+  }
+}
